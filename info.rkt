95da715557dfb1799928d524f6dfbb6223cb9ba3
ZhangHao <zhanghao@antigen.top>
2023-12-10 12:50:53 +0800

    Fix deps

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 (define collection "hasket")
-(define deps '("base"))
+(define deps '("base" "typed-racket-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/hasket.scrbl" ())))
 (define pkg-desc "自用语言")
-(define version "0.0.1")
+(define version "0.0.1.1")
 (define pkg-authors '(zhanghao))
 (define license '(Apache-2.0 OR MIT))
