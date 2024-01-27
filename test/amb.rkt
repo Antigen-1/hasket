86f14d7ccd2a09dd53867d812133643092d4d075
ZhangHao <zhanghao@antigen.top>
2024-01-27 17:29:37 +0800

    Refactor amb tests

--- test/amb.rkt
+++ test/amb.rkt
@@ -23,25 +23,15 @@
     (let cons/cached cons)
     (let check/cached check-current)
     (let reverse/cached reverse)
+    (let car/cached car)
+    (let cdr/cached cdr)
     (let null/cached null)
+    (let null?/cached null?)
+    (let list/cached list)
     (let make-queen (lambda (n) (amb (cons/cached n 1) (cons/cached n 2) (cons/cached n 3) (cons/cached n 4) (cons/cached n 5) (cons/cached n 6) (cons/cached n 7) (cons/cached n 8))))
     (let make-and-merge (lambda (n o) (let q (make-queen n)) (if (check/cached q o) (amb) (cons/cached q o))))
-    (reverse/cached
-     (make-and-merge
-      8
-      (make-and-merge
-       7
-       (make-and-merge
-        6
-        (make-and-merge
-         5
-         (make-and-merge
-          4
-          (make-and-merge
-           3
-           (make-and-merge
-            2
-            (make-and-merge 1 null/cached))))))))))
+    (let foldl (lambda (p o l) (if (null?/cached l) o (foldl p (p (car/cached l) o) (cdr/cached l)))))
+    (reverse/cached (foldl make-and-merge null/cached (list 1 2 3 4 5 6 7 8))))
    (lambda (l1)
      (displayln "Typical")
      (print-and-test
