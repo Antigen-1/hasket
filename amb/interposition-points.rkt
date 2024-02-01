9ad4a7c5148ce300ba1387430168763f3f479735
ZhangHao <zhanghao@antigen.top>
2024-02-01 10:21:27 +0800

    Refactor Amb

--- amb/interposition-points.rkt
+++ amb/interposition-points.rkt
@@ -6,18 +6,21 @@
 
   (define-syntax-parse-rule (amb choice ...)
     (append choice ...))
-  ;; 只有当执行primitive函数或遇到分支时才会真正进行组合
-  ;; -------------------------------------------
+
+  ;; n:#%app : L (a ... -> L a) (L a) ... -> L a
   (define-syntax-parse-rule (n:#%app proc arg ...)
-    (bindM proc (lambda (p) (call-all p (list arg ...)))))
+    (apply-amb-procedure proc (list arg ...)))
+  ;; n:if : (L a) (L a) (L a) -> L a
   (define-syntax-parse-rule (n:if test then else)
     (bindM test (lambda (t) (if t then else))))
-  ;; -------------------------------------------
+
   (define-syntax-parse-rule (n:quote datum)
     (unitL (quote datum)))
   (define-syntax-parse-rule (n:lambda (arg ...) body ...)
     (unitL (wrap (lambda (arg ...) body ...))))
-  (define-syntax-parse-rule (n:let name expr body ...)
-    (bindM expr ((lambda (name) body ...) . unitL)))
   (define-syntax-parse-rule (n:#%top . v)
-    (unitL v)))
+    (unitL v))
+
+  ;; Sugar
+  (define-syntax-parse-rule (n:let name expr body ...)
+    (n:#%app (n:lambda (name) body ...) expr)))
--- amb/procedure.rkt
+++ amb/procedure.rkt
@@ -1,25 +1,31 @@
 (module procedure "../base/main.rkt"
   (require (submod racket/performance-hint begin-encourage-inline) racket/list)
-  (provide wrap call-all amb-apply n:procedure?)
+  (provide wrap apply-amb-procedure amb-apply n:procedure?)
   ;; Inline instructions
   (begin-encourage-inline
     (struct wrapper (procedure) #:constructor-name wrap)
 
-    (define (call-all proc args)
-      (define call (curry/n apply 2))
-      (define (call-primitive procedure args)
-        (bindM (apply cartesian-product args)
-               (compose1 unitL (call procedure))))
-      (define (call-wrapped procedure args)
-        (bindM (apply cartesian-product args)
-               (compose1 (call (wrapper-procedure procedure)) (mapM unitL))))
+    (define (apply-amb-procedure proc args)
 
-      ((if (wrapper? proc) call-wrapped call-primitive) proc args))
+      (define (call-all proc args)
+        (define call (curry/n apply 2))
+        (define (call-primitive procedure args)
+          (bindM (apply cartesian-product args)
+                 (compose1 unitL (call procedure))))
+        (define (call-wrapped procedure args)
+          (bindM (apply cartesian-product args)
+                 (compose1 (call (wrapper-procedure procedure)) (mapM unitL))))
+
+        ((if (wrapper? proc) call-wrapped call-primitive) proc args))
+
+      (bindM proc (lambda (p) (call-all p args))))
 
     (define (n:procedure? v)
       (or (procedure? v) (wrapper? v)))
 
+    ;; Used in amb-begin
     (define amb-apply
       (wrap
        (lambda (proc args)
-         (bindM proc (lambda (p) (bindM args (lambda (a) (call-all p (mapM unitL a)))))))))))
+         (bindM args (lambda (as) (apply-amb-procedure proc (mapM unitL as)))))))
+    ))
